df92b26bc72fca3f98586f7ce893281202999026
Mikhail Shevchuk <sheb70017@gmail.com>
2020-06-07 20:54:47 +0300

    I have done HW

---
+++ lab_5/my-app/build/precache-manifest.47ffcb87686467fe30439554a4fee11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97df7838d61b94862c3f3f92d1d958cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "065f1cf4097127e987ed",
+    "url": "/static/css/main.f770d86d.chunk.css"
+  },
+  {
+    "revision": "8d69d13f956b74cf7505",
+    "url": "/static/js/2.684827d5.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.684827d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065f1cf4097127e987ed",
+    "url": "/static/js/main.9af81073.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
